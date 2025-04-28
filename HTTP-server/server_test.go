1e2c6b3768c44d449304e1d0b7dfb5ae2f020b93
Batuhan <anhtubui574@gmail.com>
2025-04-28 13:26:51 +0700

    added intergration test and make InMemoryPlaerScore records win and get player's score instead of hardcoding, the program now stores data as long as it runs

--- HTTP-server/server_test.go
+++ HTTP-server/server_test.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 type StubPlayerStore struct {
-	scores map[string]int
+	scores   map[string]int
+	winCalls []string
 }
 
 func (s *StubPlayerStore) GetPlayerScore(name string) int {
@@ -15,9 +17,29 @@ func (s *StubPlayerStore) GetPlayerScore(name string) int {
 	return score
 }
 
+func (s *StubPlayerStore) RecordWin(name string) {
+	s.winCalls = append(s.winCalls, name)
+}
+
+func TestRecordingWinsAndRetrievingThem(t *testing.T) {
+	store := NewInMemoryPlayerStore()
+	server := PlayerServer{store}
+	player := "Pepper"
+
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+
+	response := httptest.NewRecorder()
+	server.ServeHTTP(response, newGetScoreRequest(player))
+	assertStatus(t, response.Code, http.StatusOK)
+
+	assertResponseBody(t, response.Body.String(), "3")
+}
+
 func TestGETPlayers(t *testing.T) {
 	store := StubPlayerStore{
-		map[string]int{
+		scores: map[string]int{
 			"Pepper": 20,
 			"Floyd":  10,
 		},
@@ -51,15 +73,31 @@ func TestGETPlayers(t *testing.T) {
 
 func TestScoreWins(t *testing.T) {
 	store := StubPlayerStore{
-		map[string]int{},
+		scores: map[string]int{},
 	}
 	server := &PlayerServer{store: &store}
-	t.Run("returns accepted on POST", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodPost, "/players/Pepper", nil)
+	//t.Run("returns accepted on POST", func(t *testing.T) {
+	//	request, _ := http.NewRequest(http.MethodPost, "/players/Pepper", nil)
+	//	response := httptest.NewRecorder()
+	//	server.ServeHTTP(response, request)
+	//
+	//	assertStatus(t, response.Code, http.StatusAccepted)
+	//})
+	t.Run("it records wins when POST", func(t *testing.T) {
+		player := "Pepper"
+		request := newPostWinRequest("Pepper")
 		response := httptest.NewRecorder()
+
 		server.ServeHTTP(response, request)
 
 		assertStatus(t, response.Code, http.StatusAccepted)
+
+		if len(store.winCalls) != 1 {
+			t.Errorf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
+		}
+		if store.winCalls[0] != player {
+			t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], player)
+		}
 	})
 }
 
@@ -68,6 +106,11 @@ func assertStatus(t testing.TB, got, want int) {
 	if got != want {
 		t.Errorf("did not get correct status, got %d, want %d", got, want)
 	}
+}
+
+func newPostWinRequest(name string) *http.Request {
+	req, _ := http.NewRequest(http.MethodPost, fmt.Sprintf("/players/%s", name), nil)
+	return req
 }
 
 func newGetScoreRequest(name string) *http.Request {
