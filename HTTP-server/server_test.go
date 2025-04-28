58bc718b488fe54cd6b9a9831f9c02c9472bbb81
Batuhan <anhtubui574@gmail.com>
2025-04-28 22:06:22 +0700

    introduce constant for application/json content type

--- HTTP-server/server_test.go
+++ HTTP-server/server_test.go
@@ -151,13 +151,9 @@ func TestLeague(t *testing.T) {
 		request := newLeagueRequest()
 		response := httptest.NewRecorder()
 		server.ServeHTTP(response, request)
-
-		if response.Result().Header.Get("content-type") != "application/json" {
-			t.Errorf("response did not have content-type of application/json, got %v", response.Result().Header)
-		}
-
 		got := getLeagueFromResponse(t, response.Body)
 		assertStatus(t, response.Code, http.StatusOK)
+		assertContentType(t, response, jsonContentType)
 		assertLeague(t, got, wantedLeague)
 	})
 }
@@ -205,5 +201,12 @@ func assertResponseBody(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertContentType(t testing.TB, response *httptest.ResponseRecorder, want string) {
+	t.Helper()
+	if response.Result().Header.Get("content-type") != want {
+		t.Errorf("response did not have content-type of %s, got %v", want, response.Result().Header)
 	}
 }
