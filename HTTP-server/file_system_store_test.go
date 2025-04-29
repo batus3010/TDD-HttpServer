89ad5e6b493873413fb21869c5fd16e8d9e41308
Batuhan <anhtubui574@gmail.com>
2025-04-29 14:38:13 +0700

    handle sorting the returned league

--- HTTP-server/file_system_store_test.go
+++ HTTP-server/file_system_store_test.go
@@ -6,19 +6,27 @@ import (
 )
 
 func TestFileSystemStore(t *testing.T) {
-	t.Run("league from a reader", func(t *testing.T) {
+	t.Run("get league from a reader and it's sorted", func(t *testing.T) {
 		database, cleanDatabase := createTempFile(t, `[
-			{"Name": "Cleo", "Wins": 10},
-			{"Name": "Chris", "Wins": 33}]`)
+		{"Name": "Cleo", "Wins": 10},
+		{"Name": "Chris", "Wins": 33}]`)
 		defer cleanDatabase()
+
 		store, err := NewFileSystemPlayerStore(database)
+
 		assertNoError(t, err)
+
 		got := store.GetLeague()
-		got = store.GetLeague()
-		want := []Player{
-			{"Cleo", 10},
+
+		want := League{
 			{"Chris", 33},
+			{"Cleo", 10},
 		}
+
+		assertLeague(t, got, want)
+
+		// read again
+		got = store.GetLeague()
 		assertLeague(t, got, want)
 	})
 
