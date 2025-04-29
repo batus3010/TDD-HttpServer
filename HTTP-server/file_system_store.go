8f15c4fd6f815760c9c6b00fa7c820a7f6c8979f
Batuhan <anhtubui574@gmail.com>
2025-04-29 13:24:56 +0700

    created a new type 'tape' to encapsulate 'when write to file go from the beginning', Write encapsulates Seek, changed FileSystemStore to have reference to Writer, removed the Seek call from RecordWin as tape now covers it

--- HTTP-server/file_system_store.go
+++ HTTP-server/file_system_store.go
@@ -6,7 +6,7 @@ import (
 )
 
 type FileSystemPlayerStore struct {
-	database io.ReadWriteSeeker
+	database io.Writer
 	league   League
 }
 
@@ -14,7 +14,7 @@ func NewFileSystemPlayerStore(database io.ReadWriteSeeker) *FileSystemPlayerStor
 	database.Seek(0, io.SeekStart)
 	league, _ := NewLeague(database)
 	return &FileSystemPlayerStore{
-		database: database,
+		database: &tape{database},
 		league:   league,
 	}
 }
@@ -40,6 +40,5 @@ func (f *FileSystemPlayerStore) RecordWin(name string) {
 	} else {
 		f.league = append(f.league, Player{name, 1})
 	}
-	f.database.Seek(0, io.SeekStart)
 	json.NewEncoder(f.database).Encode(f.league)
 }
