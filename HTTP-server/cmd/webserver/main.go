dd21dfbf6489be3cd1ba7c6a6bb068acac6d8c9d
Batuhan <anhtubui574@gmail.com>
2025-04-29 20:42:26 +0700

    refactored both cli and webserver packages to use the new method FileSystemPlayerStoreFromFile to better handle opening file from a path

--- HTTP-server/cmd/webserver/main.go
+++ HTTP-server/cmd/webserver/main.go
@@ -4,24 +4,19 @@ import (
 	poker "HTTP-server"
 	"log"
 	"net/http"
-	"os"
 )
 
 const dbFileName = "game.db.json"
 
 func main() {
+	store, closeFunc, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer closeFunc()
 
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
-	if err != nil {
-		log.Fatalf("Problem opening %s %v", dbFileName, err)
-	}
-	store, err := poker.NewFileSystemPlayerStore(db)
-	if err != nil {
-		log.Fatalf("problem creating file system player store, %v ", err)
-	}
 	server := poker.NewPlayerServer(store)
-
 	if err := http.ListenAndServe(":5000", server); err != nil {
-		log.Fatalf("Could not listen on port 5000 %v", err)
+		log.Fatalf("could not listen on port 5000 %v", err)
 	}
 }
