68532e7209b1688baced5ece9da9f4936421b188
Batuhan <anhtubui574@gmail.com>
2025-05-04 19:29:27 +0700

    finished logic for informing increasing blind value, keeping track of how much blind value currently is

--- HTTP-server/cmd/cli/main.go
+++ HTTP-server/cmd/cli/main.go
@@ -17,5 +17,5 @@ func main() {
 	defer closeFunc()
 	fmt.Println("Let's play poker")
 	fmt.Println("Type {Name} wins to record a win")
-	poker.NewCLI(store, os.Stdin).PlayPoker()
+	poker.NewCLI(store, os.Stdin, poker.BlindAlerterFunc(poker.StdOutAlerter)).PlayPoker()
 }
