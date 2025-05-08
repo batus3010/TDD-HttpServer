1006de983c31d09008d483855ed07793cbcb0da2
Batuhan <anhtubui574@gmail.com>
2025-05-08 10:03:41 +0700

    refactored, introduce new helper method

--- HTTP-server/server_test.go
+++ HTTP-server/server_test.go
@@ -134,12 +134,17 @@ func TestLeague(t *testing.T) {
 func TestGame(t *testing.T) {
 	t.Run("GET /game returns status 200", func(t *testing.T) {
 		server := NewPlayerServer(&StubPlayerStore{})
-		request, _ := http.NewRequest(http.MethodGet, "/game", nil)
+		request := newGameRequest()
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
 		assertStatus(t, response.Code, http.StatusOK)
 	})
+}
+
+func newGameRequest() *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, "/game", nil)
+	return req
 }
 
 func assertLeague(t testing.TB, got, want []Player) {
